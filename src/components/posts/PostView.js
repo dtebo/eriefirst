9717afbdb17a95a46082a69894ad9e09a4607eb1
dtebo <darrentebo83@outlook.com>
2021-09-18 15:28:55 -0400

    reorganized components so that postview displays the full blog post

--- src/components/posts/PostView.js
+++ src/components/posts/PostView.js
@@ -1,17 +1,9 @@
 import React from 'react';
-import * as MUI from '../../MaterialUI';
-import './posts.css';
 
 const PostView = (props) => {
     return (
         <>
-            <MUI.Card className='card'>
-                <MUI.CardContent>
-                    <MUI.Typography>
-                        {props.post.title.rendered}
-                    </MUI.Typography>
-                </MUI.CardContent>
-            </MUI.Card>
+            
         </>
     );
 };
