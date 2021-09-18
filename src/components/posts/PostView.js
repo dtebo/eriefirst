bbb3394030699565bd8f196262e5367e07defb4c
dtebo <darrentebo83@outlook.com>
2021-09-18 15:55:59 -0400

    implemented full blog post view. bug exists where content doesn't consistently render

--- src/components/posts/PostView.js
+++ src/components/posts/PostView.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
 
 const PostView = (props) => {
+    const [post, setPost] = useState();
+
+    useEffect(() => {
+        const slug = props.match.params.slug;
+
+        axios.get(`https://eriefirst.org/wp-json/wp/v2/posts?slug=${slug}`)
+            .then(post => {
+                console.log(post);
+                setPost(post.data[0]);
+            })
+            .catch(err => console.error(err));
+    }, [props.match.params.slug]);
+
+    const createMarkup = (html) => {
+        return {__html: html};
+    };
+
     return (
-        <>
-            
-        </>
+        /* FIX BUG WHERE CONTENT DOESN'T CONSISTENTLY RENDER */
+        <div>
+            <h1>{post && post.title.rendered}</h1>
+            <div
+                dangerouslySetInnerHTML={createMarkup(post.content.rendered)}
+            />
+        </div>
     );
 };
 
